a2cf3d14b7438aa92c3bad44ec3c92ee0042f3c7
agitoreiken <agitoreiken@gmail.com>
2022-05-05 01:28:32 +0900

    revert

--- VSThemeProject2/file.cpp
+++ VSThemeProject2/file.cpp
@@ -4,7 +4,7 @@
 struct SynProstitutkiGalimoy;
 
 typedef SynProstitutkiGalimoy You;
-template<typename T, typename X = std::nullptr_t>
+template<typename T, Typename X = std::nullptr_t>
 X Template(T* x) {
 	return &x;
 }
@@ -12,7 +12,6 @@ X Template(T* x) {
 /*
 * Comment
 */
-static constexpr inline volatile const unsigned long long int& GlobalVariable = 0 + 1 / 2 * 5 % 6;
 
 namespace Namespace  {
 	class Derived {
@@ -24,19 +23,12 @@ namespace Namespace  {
 	class Class : public Derived {
 	 
 	public:
-		Class(...) {}
-		Class(int NahuyPoshel, char Petuh) {}
 		Class() {};
-		static Function() {
-		}
-		MACRO(x, y);
-		bool Load(int SomeVariable, Derived derived, ...) {
-			int i1 = 1234567890;
-			GlobalVariable;
-			SomeVariable;
-			// Comment
-			i1;
 
+		MACRO(x, y);
+		bool Load() {
+
+			int i1 = 1234567890;
 			int i2 = 1234567890ul;
 			int i3 = (int)'\001';
 			auto* chars = "Chars\n";
@@ -54,11 +46,10 @@ namespace Namespace  {
 			{
 
 			};
-			typedef unsigned char BYTE;
-			constexpr int x = (int)((BYTE)0x1Ell / (int)255);
+			constexpr int x = 0x1E / 255;
 
 			"\n";
-			"Пошел \t\r\nнахуй \rлох \nххахаха))))))))))\r\n000000000000\n";
+			"nnn\n";
 			"\001";
 			this->Load()
 			this->Method();
@@ -71,18 +62,11 @@ namespace Namespace  {
 Namespace::Class::Load(x);
 int main(int x, char** args, ...)  {
 	
-	Namespace::Class::Function();
+	Namespace::
 	x = x + 1;
 	auto newClass = Namespace::Class();
 	void* null = std::nullptr_t;
 	auto __ = new char('\000');
 	auto local = int(1);
 	newClass.Load();
-	(&newClass)->Load();
-	((Namespace::Class*)((void*)(&newClass)))->Load();
-	
-	auto lambda = [=, this, &newClass]() -> bool {
-		return true;
-	}
-	return false;
 }
