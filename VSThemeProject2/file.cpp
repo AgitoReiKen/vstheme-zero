67aed53523e510e3543afc96f5317ef454f6146c
agitoreiken <agitoreiken@gmail.com>
2022-05-04 10:58:33 +0900

    Add project files.

---
+++ VSThemeProject2/file.cpp
@@ -0,0 +1,70 @@
+﻿
+#include <stdio.h>
+#define MACRO(x, y)
+struct SynProstitutkiGalimoy;
+
+typedef SynProstitutkiGalimoy You;
+template<typename T, Typename X = std::nullptr_t>
+X Template(T* x) {
+	return &x;
+}
+
+/*
+* Comment
+*/
+
+namespace Namespace  {
+	class Derived {
+	public:
+		void Method() {}
+		int Property;
+	};
+	MACRO(x, y);
+	class Class : public Derived {
+	 
+	public:
+		Class() {};
+
+		MACRO(x, y);
+		bool Load() {
+
+			int i1 = 1234567890;
+			int i2 = 1234567890ul;
+			int i3 = (int)'\001';
+			auto* chars = "Chars\n";
+			auto* chars = "12321";
+			if (i != 0 || i == 1) {
+				i++;
+				i--; 
+			}
+			switch (i1) {
+				case 0: {};
+				case 1: {};
+				default: {};
+			}
+			__interface IInterface
+			{
+
+			};
+
+			"\n";
+			"nnn\n";
+			"\001";
+			this->Load()
+			this->Method();
+			this->Property;
+		}
+	};
+
+	auto __ = new char('\000');
+}
+
+
+Namespace::Class::Load(x);
+int main(int x, char** args, ...)  {
+	 
+	Namespace::Derived
+	auto newClass = Namespace::Class();
+	void* null = std::nullptr_t;
+	newClass.Load();
+}
